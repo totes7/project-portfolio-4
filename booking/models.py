62c2d3c4878f96353734792b7cf893e4997fc3fd
totes7 <antonio.manno.7@gmail.com>
2023-03-12 20:51:53 +0000

    Complete models creation for booking, locations and tables

--- booking/models.py
+++ booking/models.py
@@ -1,3 +1,35 @@
 from django.db import models
+from django.contrib.auth.models import User
+from location.models import Location, Table
+from datetime import date
 
-# Create your models here.
+
+class Booking(models.Model):
+    """ Model to place reservation """
+
+    # Choices for the time of booking
+    BOOKING_TIME = ((1, "12:00 - 14:00"),
+                    (2, "14:00 - 16:00"),
+                    (3, "18:00 - 20:00"),
+                    (4, "20:00 - 22:00"))
+
+    id = models.BigAutoField(primary_key=True)
+    customer = models.ForeignKey(User, on_delete=models.CASCADE,
+                                 related_name='booking_customer')
+    name = models.CharField(max_length=50)
+    email = models.EmailField(max_length=50)
+    location = models.ForeignKey(Location, on_delete=models.CASCADE,
+                                 related_name='location_name')
+    date = models.DateField(default=date.today)
+    time = models.IntegerField(choices=BOOKING_TIME, default=1)
+    guests = models.IntegerField(default=1)
+    special_request = models.TextField(max_length=200, blank=True)
+    table_id = models.ForeignKey(Table, on_delete=models.CASCADE,
+                                 related_name='booked_table')
+
+    class Meta:
+        """ Order bookings by date """
+        ordering = ['date']
+
+    def __str__(self):
+        return self.id
--- location/models.py
+++ location/models.py
@@ -1,3 +1,32 @@
 from django.db import models
 
-# Create your models here.
+
+class Location(models.Model):
+    """ Model for restaurant location """
+
+    city = models.CharField(max_length=25, primary_key=True)
+    country = models.CharField(max_length=25)
+    address = models.CharField(max_length=50)
+
+    class Meta:
+        """ Order alphabetically """
+        ordering = ['city']
+
+    def __str__(self):
+        return self.city
+
+
+class Table(models.Model):
+    """ Model for table booked """
+
+    id = models.BigAutoField(primary_key=True)
+    number = models.IntegerField()
+    capacity = models.IntegerField(default=2)
+
+    class Meta:
+        """ Order tables by number """
+        ordering = ['number']
+
+    def __str__(self):
+        return self.number
+
